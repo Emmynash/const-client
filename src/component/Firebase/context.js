36627ffbf4f2f26a4da797ec3d2c884f586a0050
Akita Emmanuel <e.akita91@gmail.com>
2020-12-17 08:35:46 +0100

    Wrap firebaseContext in higher order component

--- src/component/Firebase/context.js
+++ src/component/Firebase/context.js
@@ -5,7 +5,7 @@ import React from 'react';
 const firebaseContext = React.createContext(null);
 
 // A higher order component to handle the context
-export const withFirebase = (Component: any) => (props: any) => (
+export const withFirebase = (Component) => (props) => (
   <firebaseContext.Consumer>
   { firebase => <Component { ...props } firebase = { firebase } />}
 </firebaseContext.Consumer>
