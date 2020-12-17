7391d250498ae6cfccdd67721ee3c7607422a1f0
Akita Emmanuel <e.akita91@gmail.com>
2020-12-17 20:13:58 +0100

    change color code to #000

--- src/component/Auth/logout.js
+++ src/component/Auth/logout.js
@@ -3,7 +3,7 @@ import { Button } from "reactstrap";
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <Button outline color="secondary" style={{color: "#ef5635"}} onClick={firebase.doSignOut}>
+  <Button outline color="secondary" style={{color: "#000"}} onClick={firebase.doSignOut}>
     Sign Out
   </Button>
 );
