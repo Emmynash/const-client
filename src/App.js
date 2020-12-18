fa7ef70da0916caf482774c1964a5b23e1039471
Akita Emmanuel <e.akita91@gmail.com>
2020-12-18 05:09:45 +0100

    Add EditOrder route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { withAuthentication, AuthUserContext } from './component/session';
+import { Route, Switch } from 'react-router-dom';
+import { withAuthentication } from './component/session';
 import Login from './component/Auth/login';
-import Order from './component/Order'
+import {  EditOrder, Order } from './component/Order'
 import ErrorPage from './container/404';
 import Layout from './component/Layout';
 
@@ -11,10 +11,12 @@ function App() {
     <div>
       <Layout>
         <Switch>
-        <Route path='/order' exact component={Order} />
-        <Route path='/' exact component={Login} /> 
-        <Route component={ErrorPage} />
-          </Switch>
+          <Route path='/' exact component={Login} />
+          <Route path='/order' exact component={Order} />
+          <Route path='/order/{uid}' exact component={Order} /> 
+          <Route path='/order/:uid/edit' exact component={EditOrder} /> 
+          <Route component={ErrorPage} />
+        </Switch>
       </Layout>
     </div>
   );
